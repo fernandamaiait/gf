3ea6babd353cf2c43f405dc5acaeb5e040b8c3b3
fernandamaiabettha <fernanda.maia@bettha.com>
2023-07-22 15:58:25 -0300

    feat: improve Card - design system component

--- src/design-system/Card/Card.tsx
+++ src/design-system/Card/Card.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
-export default function Card() {
-  return <div></div>;
+export default function Card({ children }: { children: ReactNode }) {
+  return <div className="shadow w-full p-8">{children}</div>;
 }
